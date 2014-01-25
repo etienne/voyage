2df6ae1437815e1d4b69e9b16f0d5c282021c0a7
Étienne Després <etienne.despres@molotovcoop.org>
2014-01-25 21:16:41 +0000

    Ajouté des posts. Modifié des posts. Shimmé des choses.

--- source/javascripts/posts.js
+++ source/javascripts/posts.js
@@ -1,7 +1,19 @@
-$(".main").onepage_scroll({
-  afterMove: function(index) {
-    // window.setTimeout(function() {
-    //   $('span.button').click();
-    // }, 410);
-  }
+lights_timeout = null;
+
+$(function() {
+  $('span.link').click(function() {
+    var slug = $(this).attr('data-slug');
+    var i = $('#' + slug).index() + 1;
+    $('.main').moveTo(i);
+  });
+  
+  $('#lumiere').mousemove(function() {
+    $(this).removeClass('off');
+    if (lights_timeout != null) {
+      window.clearTimeout(lights_timeout);
+    }
+    lights_timeout = window.setTimeout(function() {
+      ($('#lumiere').addClass('off'));
+    }, 8000);
+  });
 });
---
+++ source/posts/2013-11-08-colimacon.html.md
@@ -0,0 +1,4 @@
+---
+date: 2013-11-08 20:00:00
+description: Escalier de Templiers au Convento Cristo, Tomar.
+---
